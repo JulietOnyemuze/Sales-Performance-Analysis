14b4312d5928b7d8b8e773bdb08d1a9f25a0619f
JulietOnyemuze <chiagoziejuliet015@gmail.com>
2026-03-21 15:13:42 +0100

    Add files via upload

---
+++ Sql Script Fact_InternetSales.sql
@@ -0,0 +1,38 @@
+-- Cleaned FACT_InternetSales Table --
+SELECT
+    [ProductKey],
+    [OrderDateKey],
+    [DueDateKey],
+    [ShipDateKey],
+    [CustomerKey],
+--      [PromotionKey]
+--      [CurrencyKey]
+--      [SalesTerritoryKey]
+    [SalesOrderNumber],
+--      [SalesOrderLineNumber]
+--      [RevisionNumber]
+--      [OrderQuantity]
+--      [UnitPrice]
+--      [ExtendedAmount]
+--      [UnitPriceDiscountPct]
+--      [DiscountAmount]
+--      [ProductStandardCost]
+--      [TotalProductCost]
+     [SalesAmount]
+--      [TaxAmt]
+--      [Freight]
+--      [CarrierTrackingNumber]
+--      [CustomerPONumber]
+--      [OrderDate]
+--      [DueDate]
+--      [ShipDate]
+FROM
+   [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE
+    LEFT (OrderDateKey, 4) >= 2019    --Ensure we only bring two years of date from extraction
+ORDER BY
+    OrderDateKey ASC
+
+
+
+
